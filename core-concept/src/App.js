b01bfc19b6dd6cc091fa3740bdc293fd92c3fc95
salahuddin867 <salahuddin867@gmail.com>
2021-07-17 19:59:04 +0600

    core concepts exposed

--- core-concept/src/App.js
+++ core-concept/src/App.js
@@ -2,24 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Person />
+
+
       </header>
     </div>
   );
 }
+function Person() {
+  const nayoks = ['Razzak', 'Salman', 'Sakib', 'Shuvo']
+  const products = [
+    { name: 'Photoshop', Price: 'TK.1000' },
+    { name: 'Illustrator', Price: 'TK.999' },
+    { name: 'PDF Reader', Price: 'TK.900' },
+    { name: 'PDF', Price: 'TK.90' },
+    { name: 'PF', Price: 'TK.9' },
+  ]
+  return <div>
+    <h1>Nayoks:</h1>
+    <ul>
+      {
+        nayoks.map(nayok => <li>{nayok}</li>)
+      }
+      <h2>Products:</h2>
+      {
+        products.map(product => <li>{product.name} {product.Price}</li>)
 
+      }
+
+    </ul>
+  </div>
+
+}
 export default App;
